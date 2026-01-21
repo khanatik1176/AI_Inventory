593fa26d1f3fd72a1299310875e73857f4ca07d2
Khan Atik Faisal <khanatik1176@gmail.com>
2026-01-21 19:57:19 +0600

    bug-fixed

--- backend/render_start.sh
+++ backend/render_start.sh
@@ -4,4 +4,4 @@ set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput || true
 
-gunicorn your_project_name.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
